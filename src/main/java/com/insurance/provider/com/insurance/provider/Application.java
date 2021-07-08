7406e87e39bfe05d7466bb17b236c113df5e9cac
kaknagraj <79716175+kaknagraj@users.noreply.github.com>
2021-07-08 12:35:06 +0530

    Add files via upload

---
+++ src/main/java/com/insurance/provider/com/insurance/provider/Application.java
@@ -0,0 +1,29 @@
+package com.insurance.provider.com.insurance.provider;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("insurance-service")
+public class Application {
+
+		
+	@GetMapping("/getUpdatedInsuranceTypes")
+	public List<String> getInsuranceTypes(){
+		
+		 return Stream.of("Jeevan Anand","Jeevan Beema","Jeevan Saral","Jeevan anoop","naga plan").collect(Collectors.toList());
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
